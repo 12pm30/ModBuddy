7bfc62ccff3754a415ac820c051cbfd327194623
Ameris Rudland <13akar@queensu.ca>
2017-02-04 13:59:48 -0500

    Injecting script to retreive page HTML for comment parsing

--- popup.js
+++ popup.js
@@ -1,7 +1,22 @@
+chrome.runtime.onMessage.addListener(function(request, sender){
+	if (request.action == "getSource") {
+		message.innerText = request.source;
+	}
+});
 
+function onWindowLoad() {
+	var message = document.querySelector('#message');
+	
+	chrome.tabs.executeScript(null, {
+		file: "getPagesSource.js"
+	}, function() {
+		if (chrome.runtime.lastError) {
+			message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+		}
+	});
+}
 
+window.onload = onWindowLoad;
 
-
-
-window.alert("Hello world!");
+// window.alert("Hello world!");
 
